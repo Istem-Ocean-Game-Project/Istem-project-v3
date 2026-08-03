7e2eb755815a55dff08df4acaec03efd288c259f
redac <marc.nl.heng@gmail.com>
2026-08-04 09:44:46 +1000

    clownfish are so weirdly smol for no reaosn niow

--- istem project v3 game (testing new repo)/Scripts/Hitbox.gd
+++ istem project v3 game (testing new repo)/Scripts/Hitbox.gd
@@ -1,9 +1,13 @@
 class_name TemplateHitbox
 extends Area2D
 @export var damage = 1
-
+@onready var player = get_parent().get_parent().get_parent()
 
 
 func _init() -> void:
 	collision_layer = 1024 #11
 	collision_mask = 2
+
+func _process(delta: float) -> void:
+	if player.velocity.length() >= 1000 and player.highmode:
+		damage * 1.5
