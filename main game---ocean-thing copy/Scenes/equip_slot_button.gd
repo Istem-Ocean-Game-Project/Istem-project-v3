6e95fdc31807f190887b4e99f0faab6d5a316537
Lucas Wan <lucas.wan1@education.nsw.gov.au>
2026-08-06 07:39:07 +1000

    ew

--- main game---ocean-thing copy/Scenes/equip_slot_button.gd
+++
@@ -1,9 +0,0 @@
-extends TextureButton  # could also be TextureRect now, no click needed
-
-@export var gear_type: Inventory.GearType
-
-func refresh(item_data) -> void:
-	if item_data == null:
-		pass  # show empty slot placeholder
-	else:
-		pass  # texture = load(item_data.get("icon", "res://icons/default.png"))
